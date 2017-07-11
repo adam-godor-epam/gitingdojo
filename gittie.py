69c3b75389f90101ec0d864a47ce9e4b293b22c7
Balint Mogyorosi <bg.mogyorosi@gmail.com>
2017-07-11 09:39:51 +0200

    joke function finished

--- gittie.py
+++ gittie.py
@@ -7,8 +7,14 @@ def add(a, b):
 
 
 def joke():
-    pass
-
+    print("How do trees access the internet?")
+    answer = "They log on."
+    for i in range(5):
+        if input("Answer({} tries left): ".format()) == answer:
+            print("You have the correct answer.")
+            return
+    print("You ran out from tries. The answer was: {}".format(answer))
+        
 
 def shout():
     pass
